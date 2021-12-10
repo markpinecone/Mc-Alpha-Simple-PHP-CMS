a92840505a36f4bb42668f6819612bc49e237fb3
martspriezciekurs <marts.priezciekurs@pinecone.lv>
2021-12-10 13:29:13 +0200

    Added prepared statements

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Mc-Alpha-Simple-PHP-Login
 Attempt to build very simple Login system.
+
+#ToDo:
+secure forms 
+etc.
--- includes/sign_up.inc.php
+++ includes/sign_up.inc.php
@@ -1,36 +1,49 @@
 <?php
-require INCLUDE_DIR . '/dbh.inc.php';
+require_once '../config/config.php';
+require_once INCLUDE_DIR . '/dbh.inc.php';
+require FUNCTIONS_DIR . '/sign_up.func.php';
 
 if (isset($_GET["submit"])) {
-    if ($_GET['pass'] != $_GET['repeat-pass']) {
+    if ($_GET['password'] != $_GET['repeat-password']) {
         echo 'Passwords doesn\'t match!';
     } else {
-
+        $stmt = mysqli_stmt_init($conn);
         if (isset($_GET['email'])) {
-            $email = $_GET['email'];
+            $email = $_GET["email"];
+            $password = $_GET["password"];
+            $name = $_GET["name"];
+            $lastname = $_GET["lastname"];
+            $role = 'user';
         }
-        $checkEmail = $conn->query("SELECT email FROM Users WHERE email='{$email}'");
-        //    echo var_dump($checkEmail);
-
+        $findEmail = "SELECT email FROM Users WHERE email=?";
         $insertUserQuery = "INSERT INTO Users (email, pass, name, lastname, role)
-        VALUES ('" . $_GET["email"] . "', '" . $_GET["pass"] . "', '" . $_GET["name"] . "', '" . $_GET["lastname"] . "', 'user')
-        ";
-        if ($checkEmail->num_rows == 0) {
-            try {
-                if ($conn->query($insertUserQuery) === TRUE) {
-                    echo "<p>User has been created.</p>";
-                }
-            } catch (Exception $e) {
-                echo 'Caught exception: ', $e->getMessage(), "\n";
-            }
+            VALUES (?, ?, ?, ?, ?)";
+
+        if (!mysqli_stmt_prepare($stmt, $findEmail)) {
+            echo "SQL statement failed";
         } else {
-            echo 'User with email: ' .  $email . ' already exist!';
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            $result = $stmt->get_result();
+            if ($result->num_rows == 0) {
+                if (!mysqli_stmt_prepare($stmt, $insertUserQuery)) {
+                    echo "SQL statement failed";
+                } else {
+                    mysqli_stmt_bind_param($stmt, "sssss", $email, $password, $name, $lastname, $role);
+                    mysqli_stmt_execute($stmt);
+                    echo '<div class="notify-success" User with e-mail ' . $email . ' has been added successfully';
+                }
+            }else {
+                echo '<div class="alert">User with e-mail ' . $email . ' already exist!</div>';
+            }
+
         }
 
     }
-
-    $conn->close();
-
 }
+
+
+
+
 
 
